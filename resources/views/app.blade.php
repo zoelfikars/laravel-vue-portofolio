1aa178be3b4957cd9eda602f5bef8d702ce70fcb
zoelfikars <zuulfikarfikrii@gmail.com>
2026-01-17 18:20:43 +0700

    removing css in blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Portfolio</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/js/app.js'])
 </head>
 
 <body class="bg-gray-50 dark:bg-[#0d1117] text-gray-900 dark:text-gray-100">
